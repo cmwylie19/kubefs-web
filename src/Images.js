dd58f9962ec8ff86205be55081582d2fc58ce38d
Casey Wylie <cwylie@redhat.com>
2022-11-12 08:59:37 -0500

    refactor for testing
    Signed-off-by: Casey Wylie <cwylie@redhat.com>

--- src/Images.js
+++ src/Images.js
@@ -1,67 +1,19 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
-import { GetDateRange, NameToDateInt} from "./helpers";
+import * as helpers from "./helpers";
 import "./Images.css";
 
-const filterPics = (pics, date) => {
-  if (date !== "") {
-    return pics.filter(pic => pic.Name.substring(0, date.length + 1) === "A" + date)
-  } else {
-    return pics
-  }
-}
-const fetchPics = async (setPics) => {
-  axios.get('http://192.168.1.209:30099/list')
-    .then(res => {
-      res.data.map(pic => pic.Active = false)
-      setPics(res.data)
-    })
-};
 
-const setActive = (name, pics, setPics) => {
-  pics.map(pic => {
-    if (pic.Name === name) {
-      pic.Active = true
-    }
-    return true
-  })
-  setPics(pics)
-}
-const setUnActive = (name, pics, setPics) => {
-  pics.map(pic => {
-    if (pic.Name === name) {
-      pic.Active = false;
-    }
-    return true
-  })
-  setPics(pics)
-}
 
-const deletePic = (e, path, pics, setPics, filterPics, date) => {
-  if (e.detail === 2) {
-    axios.get('http://192.168.1.209:30099/delete/file' + path)
-      .then(res => {
-        if (res.data) {
-          setPics(pics.filter(pic => pic.Path !== path));
-        }
-      })
-  } else if (e.detail === 4) {
-    axios.get(`http://192.168.1.209:30099/delete/cascade?begin=${GetDateRange(filterPics(pics, date))[0]}&end=${GetDateRange(filterPics(pics, date))[1]}`)
-    .then(res => {
-      if (res.data) {
-        setPics(pics.filter(pic => NameToDateInt(pic.Path) >=  GetDateRange(filterPics(pics, date))[0] && NameToDateInt(pic.Path) <= GetDateRange(filterPics(pics, date))[1]));
-      }
-    })
-  }
-}
+
+
 
 function Images({ date }) {
   const [pics, setPics] = useState(null);
 
   useEffect(() => {
-    fetchPics(setPics);
+    helpers.FetchPics(setPics);
     const interval = setInterval(() => {
-      fetchPics(setPics);
+      helpers.FetchPics(setPics);
     }, 10000);
     return () => clearInterval(interval);
 
@@ -69,15 +21,15 @@ function Images({ date }) {
 
   return (
     <>
-      {filterPics(pics, date) && filterPics(pics, date).map((pic) =>
+      {helpers.FilterPics(pics, date) && helpers.FilterPics(pics, date).map((pic) =>
         <img
           alt={pic.Name}
           className="image"
           key={pic.Name}
           src={"http://192.168.1.209:30099" + pic.Path.replace("/media", "")}
-          onClick={(e) => deletePic(e, pic.Path, pics, setPics, filterPics, date)}
-          onMouseOver={() => setActive(pic.Name, pics, setPics)}
-          onMouseOut={() => setUnActive(pic.Name, pics, setPics)}
+          onClick={(e) => helpers.DeletePic(e, pic.Path, pics, setPics, filterPics, date)}
+          onMouseOver={() => helpers.SetActive(pic.Name, pics, setPics)}
+          onMouseOut={() => helpers.SetUnActive(pic.Name, pics, setPics)}
         />)}
     </>
   );
--- src/helpers.js
+++ src/helpers.js
@@ -1,9 +1,64 @@
+import axios from "axios"
+
+export const FetchPics = async (setPics) => {
+    axios.get('http://192.168.1.209:30099/list')
+        .then(res => {
+            res.data.map(pic => pic.Active = false)
+            setPics(res.data)
+        })
+};
+
+const SetActive = (name, pics, setPics) => {
+    pics.map(pic => {
+      if (pic.Name === name) {
+        pic.Active = true
+      }
+      return true
+    })
+    setPics(pics)
+  }
+  const SetUnActive = (name, pics, setPics) => {
+    pics.map(pic => {
+      if (pic.Name === name) {
+        pic.Active = false;
+      }
+      return true
+    })
+    setPics(pics)
+  }
+  
+  const DeletePic = (e, path, pics, setPics, filterPics, date) => {
+    if (e.detail === 2) {
+      axios.get('http://192.168.1.209:30099/delete/file' + path)
+        .then(res => {
+          if (res.data) {
+            setPics(pics.filter(pic => pic.Path !== path));
+          }
+        })
+    } else if (e.detail === 4) {
+      axios.get(`http://192.168.1.209:30099/delete/cascade?begin=${GetDateRange(filterPics(pics, date))[0]}&end=${GetDateRange(filterPics(pics, date))[1]}`)
+      .then(res => {
+        if (res.data) {
+          setPics(pics.filter(pic => NameToDateInt(pic.Path) >=  GetDateRange(filterPics(pics, date))[0] && NameToDateInt(pic.Path) <= GetDateRange(filterPics(pics, date))[1]));
+        }
+      })
+    }
+  }
+
+export const FilterPics = (pics, date) => {
+    if (date !== "") {
+        return pics.filter(pic => pic.Name.substring(0, date.length + 1) === "A" + date)
+    } else {
+        return pics
+    }
+}
+
 export function NameToDateInt(name) {
-    return parseInt(name.split("A")[1]+name.split(".")[0]);
+    return parseInt(name.split("A")[1] + name.split(".")[0]);
 }
 
 export function DateIntToName(dateint) {
-    return "A"+dateint+".jpg";
+    return "A" + dateint + ".jpg";
 }
 
 export function NameFromPath(path) {
@@ -12,11 +67,11 @@ export function NameFromPath(path) {
 
 export function GetDateRange(arr) {
     if (Array.isArray(arr)) {
-    let dateIntArray = arr.map(pic => NameToDateInt(pic.Name));
-    console.log("DateIntArray ",JSON.stringify(dateIntArray,undefined,2))
-    dateIntArray.sort((a, b) => a - b);
-    return [DateIntToName(dateIntArray[0]),DateIntToName(dateIntArray[dateIntArray.length-1])];
-    } else { 
+        let dateIntArray = arr.map(pic => NameToDateInt(pic.Name));
+        console.log("DateIntArray ", JSON.stringify(dateIntArray, undefined, 2))
+        dateIntArray.sort((a, b) => a - b);
+        return [DateIntToName(dateIntArray[0]), DateIntToName(dateIntArray[dateIntArray.length - 1])];
+    } else {
         return []
     }
 }
