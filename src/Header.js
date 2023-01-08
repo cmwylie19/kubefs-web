d7bf4d6bcfd9e03c0b79b7e022968cfd6d9eb9b5
Casey Wylie <cwylie@redhat.com>
2023-01-08 11:55:43 -0500

    updates to frontend
    Signed-off-by: Casey Wylie <cwylie@redhat.com>

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const DisplayCount = (currentTitle, setTitle, count ) => currentTitle === "Kube-FS" ? setTitle("total: "+count) : setTitle("Kube-FS")
 export const Header = ({ date, setDate, count }) => {
-    const [color, setColor] = useState("white");
+    const [color, setColor] = useState("purple");
     const [title, setTitle] = useState("Kube-FS")
     return <header className="header" >
         <h1 onMouseDown={()=>DisplayCount(title,setTitle,count)} onMouseOver={()=>setColor("#"+Math.floor(Math.random()*16777215).toString(16))} style={{color:color}}>{title}</h1>
