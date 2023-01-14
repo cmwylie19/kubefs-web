655a007f9487b847907565099c0775a29cbaf7ff
Casey Wylie <cwylie@redhat.com>
2023-01-14 10:09:17 -0500

    adding tests
    Signed-off-by: Casey Wylie <cwylie@redhat.com>

--- src/Header.test.js
+++ src/Header.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import Root from './Root';
+import { render, fireEvent,screen } from '@testing-library/react';
+import {Header,DisplayCount} from './Header';
 
 test('renders learn react link', () => {
-  render(<Root />);
+  render(<Header />);
   const linkElement = screen.getByText(/Kube-FS/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('DisplayCount',  () => {
+  let count = 0
+  let result = DisplayCount((msg)=>"hi",count)
+
+  expect(result).toBe("total: "+count)
+})
